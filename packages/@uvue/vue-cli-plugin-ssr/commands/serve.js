f489dd384c2aee8490bc4afae18a87f6652cef37
Simon Babay <simon@babay.fr>
2018-09-01 16:59:01 +0200

    (cli-plugin) Command dev serve

---
+++ packages/@uvue/vue-cli-plugin-ssr/commands/serve.js
@@ -0,0 +1,65 @@
+const defaults = {
+  host: '127.0.0.1',
+  port: 8080,
+};
+
+module.exports = (api, options) => {
+  api.registerCommand(
+    'ssr:serve',
+    {
+      description: 'start development server (SSR)',
+      usage: 'vue-cli-service ssr:serve [options]',
+      options: {
+        '--mode': `specify env mode (default: development)`,
+        '--host': `specify host (default: ${defaults.host})`,
+        '--port': `specify port (default: ${defaults.port})`,
+      },
+    },
+    async function(args) {
+      const projectDevServerOptions = options.devServer || {};
+
+      /**
+       * Gettings host & port
+       */
+      const portfinder = require('portfinder');
+      const host = args.host || process.env.HOST || projectDevServerOptions.host || defaults.host;
+      portfinder.basePort =
+        args.port || process.env.PORT || projectDevServerOptions.port || defaults.port;
+      const port = await portfinder.getPortPromise();
+
+      /**
+       * Create server
+       */
+      const { Server } = require('@uvue/server');
+      const getWebpackConfig = require('../webpack/ssr');
+
+      console.log(host, port);
+
+      const server = new Server({
+        paths: {
+          serverBundle: 'assets/server-bundle.json',
+          clientManifest: 'assets/client-manifest.json',
+          templates: {
+            spa: 'assets/spa.html',
+            ssr: 'assets/ssr.html',
+          },
+        },
+
+        webpack: {
+          client: getWebpackConfig(api, { client: true }),
+          server: getWebpackConfig(api, { client: false }),
+        },
+
+        httpOptions: {
+          host,
+          port,
+        },
+      });
+
+      /**
+       * Start server
+       */
+      await server.start();
+    },
+  );
+};
--- packages/@uvue/vue-cli-plugin-ssr/generator/index.js
+++ packages/@uvue/vue-cli-plugin-ssr/generator/index.js
@@ -1,8 +1,12 @@
 module.exports = api => {
   api.extendPackage({
     dependencies: {
+      'vue-router': '^3.0.1',
       '@uvue/core': '^0.4.0-alpha.0',
       '@uvue/server': '^0.4.0-alpha.0',
+    },
+    scripts: {
+      'ssr:serve': 'vue-cli-service ssr:serve',
     },
   });
 };
