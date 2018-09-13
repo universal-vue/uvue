b7fc2b9fd7cfa749c9bc77630914b870cdd102f0
Simon Babay <simon@babay.fr>
2018-09-14 00:48:01 +0200

    (tests) Route to test server error

---
+++ tests/project/fixtures/src/views/ServerRouteError.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page">
+    <h1>Server route error</h1>
+
+    <p>
+      This page can not be rendered on server side.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    window.console.log('Error');
+  },
+};
+</script>
