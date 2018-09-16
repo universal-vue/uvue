ff0256d3a3a40963ac620d9a6881c23e1afdeca7
Simon Babay <simon@babay.fr>
2018-09-16 14:36:41 +0200

    (core) routeResolve catch redirects

--- packages/@uvue/core/lib/routeResolve.js
+++ packages/@uvue/core/lib/routeResolve.js
@@ -1,5 +1,6 @@
 import UVue from '@uvue/core';
 import getContext from './getContext';
+import { doRedirect, RedirectError } from '@uvue/core/lib/redirect';
 
 /**
  * Function called when a route is being resolved
@@ -12,8 +13,14 @@ export default async (context, { to, next } = {}) => {
     // Call hooks on route resolve
     await UVue.invokeAsync('routeResolve', routeContext);
   } catch (error) {
-    // Call hooks if there is an error
-    await UVue.invokeAsync('routeError', error, routeContext);
+    if (error instanceof RedirectError) {
+      // Do redirect
+      doRedirect(context, error);
+      return;
+    } else {
+      // Call hooks if there is an error
+      await UVue.invokeAsync('routeError', error, routeContext);
+    }
   }
 
   // If inside navigation guard
