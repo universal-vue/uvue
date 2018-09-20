3a2c807e06f6fb9dfe8c8f6794452c9a147ad7c1
Simon Babay <simon@babay.fr>
2018-09-20 17:56:09 +0200

    (tests) Fix demo fixtures with base route & server plugin

--- tests/demo/fixtures/src/router.js
+++ tests/demo/fixtures/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -7,6 +8,17 @@ export default () => {
   return new Router({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [],
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home,
+      },
+      {
+        path: '/about',
+        name: 'about',
+        component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      },
+    ],
   });
 };
