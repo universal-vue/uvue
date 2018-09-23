6d8cca629527ba6b81e37d99c5a12044fd0f75b2
Simon Babay <simon@babay.fr>
2018-09-23 15:47:28 +0200

    (server) Default options for plugins

--- packages/@uvue/server/plugins/gzip.js
+++ packages/@uvue/server/plugins/gzip.js
@@ -2,6 +2,7 @@ import shrinkRay from 'shrink-ray-current';
 
 export default {
   install(app) {
+    this.$options = { ...(this.$options || {}) };
     app.use(shrinkRay(this.$options));
   },
 };
--- packages/@uvue/server/plugins/static.js
+++ packages/@uvue/server/plugins/static.js
@@ -2,14 +2,13 @@ import serveStatic from 'serve-static';
 
 export default {
   install(app) {
+    this.$options = {
+      directory: 'dist',
+      options: {},
+      ...(this.$options || {}),
+    };
+
     const { options, directory } = this.$options;
-    app.use(
-      serveStatic(directory, {
-        ...{
-          index: false,
-        },
-        ...options,
-      }),
-    );
+    app.use(serveStatic(directory, options));
   },
 };
