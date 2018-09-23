6cb6375777921027d9675b03189344cffea320aa
Simon Babay <simon@babay.fr>
2018-09-23 15:08:48 +0200

    (tests) Fixtures for CSS tests

--- tests/project/fixtures/src/views/Home.vue
+++ tests/project/fixtures/src/views/Home.vue
@@ -3,3 +3,11 @@
     <h1>UVue <span class="small">- Test project</span></h1>
   </div>
 </template>
+
+<style scoped>
+h1 .small {
+  color: #666;
+  font-weight: normal;
+  font-size: 0.7em;
+}
+</style>
