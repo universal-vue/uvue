588fa8b00bcfcaf2cd93c26190ebb7d678f75f09
Simon Babay <simon@babay.fr>
2018-12-11 15:00:19 +0100

    (server) Cleaning render middlewares

--- packages/@uvue/server/src/adapters/KoaAdapter.ts
+++ packages/@uvue/server/src/adapters/KoaAdapter.ts
@@ -48,61 +48,11 @@ export class KoaAdapter extends ConnectAdapter {
   // @ts-ignore
   public async renderMiddleware(ctx: any) {
     const { req, res } = ctx;
-
-    const response: IResponseContext = this.createResponseContext(req, res);
-    const context: IRequestContext = this.createRequestContext(ctx);
-
-    try {
-      // Hook before render
-      await this.uvueServer.invokeAsync('beforeRender', context, this);
-
-      if (!res.finished) {
-        const { spaPaths } = this.uvueServer.options;
-
-        if (spaPaths && spaPaths.length && micromatch.some(context.url, spaPaths)) {
-          // SPA paths
-
-          response.body = await this.uvueServer.renderer.renderSPAPage();
-        } else {
-          // SSR Process
-
-          // Render page body
-          response.body = await this.uvueServer.renderer.render(context);
-
-          // Check if there is a redirection
-          if (context.redirected) {
-            return;
-          }
-
-          // Hook before building the page
-          await this.uvueServer.invokeAsync('beforeBuild', response, context, this);
-
-          // Build page
-          response.body = await this.uvueServer.renderer.renderSSRPage(response.body, context);
-        }
-
-        // Hook on rendered
-        await this.uvueServer.invokeAsync('rendered', response, context, this);
-      }
-    } catch (err) {
-      // Catch errors
-      await this.uvueServer.invokeAsync('routeError', err, response, context, this);
-    }
-
-    // Send response
-    this.send(response, context);
-
-    // Hook after response was sent
-    this.uvueServer.invoke('afterResponse', context, this);
-
-    return {
-      context,
-      response,
-    };
+    return super.renderMiddleware(req, res);
   }
 
   protected createRequestContext(ctx: any): IRequestContext {
-    const { req, res, cookies } = ctx;
+    const { req, res } = ctx;
     const context = super.createRequestContext(req, res);
 
     context.inject = {
