0b23360235eae9f0d556eb304696dc519b640aed
Simon Babay <simon@babay.fr>
2019-06-02 15:46:23 +0200

    (core) Precompile scripts finished

---
+++ packages/@uvue/core/build.js
@@ -0,0 +1,49 @@
+const rollup = require('rollup');
+const babel = require('rollup-plugin-babel');
+const minify = require('rollup-plugin-babel-minify');
+const sourcemaps = require('rollup-plugin-sourcemaps');
+
+const libs = [
+  // Core
+  ['src/index.js', 'dist/index.js'],
+  ['src/client.js', 'dist/client.js'],
+  ['src/server.js', 'dist/server.js'],
+  // Plugins
+  ['src/plugins/apollo.js', 'plugins/apollo.js'],
+  ['src/plugins/asyncData.js', 'plugins/asyncData.js'],
+  ['src/plugins/errorHandler.js', 'plugins/errorHandler.js'],
+  ['src/plugins/middlewares.js', 'plugins/middlewares.js'],
+  ['src/plugins/vuex.js', 'plugins/vuex.js'],
+];
+
+(async () => {
+  const builds = [];
+  for (const lib of libs) {
+    const [from, to] = lib;
+    builds.push(buildLib(from, to));
+  }
+
+  await Promise.all(builds);
+})();
+
+async function buildLib(from, to) {
+  const bundle = await rollup.rollup({
+    input: from,
+    plugins: [
+      sourcemaps(),
+      babel({
+        runtimeHelpers: true,
+        sourceMaps: 'both',
+      }),
+      minify({
+        comments: false,
+      }),
+    ],
+  });
+
+  await bundle.write({
+    file: to,
+    format: 'esm',
+    sourcemap: true,
+  });
+}
--- scripts/postinstall.cmd
+++ scripts/postinstall.cmd
@@ -1,5 +1,7 @@
 cd packages/@uvue/server
 call yarn build
+cd ../core
+call yarn build
 cd ../rquery
 call yarn build
 cd ../../..
