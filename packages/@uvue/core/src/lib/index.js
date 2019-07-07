d85d67904c5020eb96b91c952c103210994f5fd8
Simon Babay <simon@babay.fr>
2019-07-08 01:37:15 +0200

    (misc) Marker

--- packages/@uvue/core/src/lib/index.js
+++ packages/@uvue/core/src/lib/index.js
@@ -1,9 +1,12 @@
+import Vue from 'vue';
 import UVue from './UVue';
 import getContext from './getContext';
 import initApp from './initApp';
 import onHotReload from './onHotReload';
 import routeResolve from './routeResolve';
 import sanitizeComponent from './sanitizeComponent';
+
+Vue.__UVUE__ = true;
 
 export { UVue, getContext, initApp, onHotReload, routeResolve, sanitizeComponent };
 export * from './catchError';
