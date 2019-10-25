2470e5c0707015c52d07cb2190ede5e8f1316249
Simon Babay <simon@babay.fr>
2019-10-25 11:03:13 +0200

    (apollo) Use builtin serialize function

--- packages/@uvue/core/src/plugins/apollo.js
+++ packages/@uvue/core/src/plugins/apollo.js
@@ -10,8 +10,8 @@ export default {
   async sendSSRData({ app, ssr, error }) {
     if (process.server) {
       try {
-        ssr.bodyAdd = `<script>window.__APOLLO_STATE__=${JSON.stringify(
-          ApolloSSR.getStates(app.$apolloProvider),
+        ssr.bodyAdd = `<script>window.__APOLLO_STATE__=${ApolloSSR.serializeStates(
+          app.$apolloProvider,
         )}</script>`;
       } catch (err) {
         error(err);
