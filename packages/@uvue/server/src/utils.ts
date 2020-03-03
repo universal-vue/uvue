686f5fbb9378c6574ae6781201db2bf5c313bf5c
Simon Babay <simon@babay.fr>
2020-03-03 15:02:18 +0100

    (deps) Update

--- packages/@uvue/server/src/utils.ts
+++ packages/@uvue/server/src/utils.ts
@@ -2,9 +2,9 @@ import fastSafeStringify from 'fast-safe-stringify';
 
 const UNSAFE_CHARS_REGEXP = /[<>\/\u2028\u2029]/g;
 const ESCAPED_CHARS = {
+  '/': '\\u002F',
   '<': '\\u003C',
   '>': '\\u003E',
-  '/': '\\u002F',
   '\u2028': '\\u2028',
   '\u2029': '\\u2029',
 };
